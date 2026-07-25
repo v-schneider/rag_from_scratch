586ecede40f857a061e5f16487217f400d7e5b0d
v-schneider <valentin.schneider@mail.de>
2026-07-25 22:35:06 +0200

    First version with FAISS and Ollama

---
+++ rag_from_scratch.py
@@ -0,0 +1,51 @@
+from bs4.filter import SoupStrainer
+
+from langchain import hub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_ollama import ChatOllama, OllamaEmbeddings
+
+#### INDEXING ####
+
+# Load Documents
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=SoupStrainer(
+            class_=("post-content", "post-title", "post-header")
+        )
+    )
+)
+docs = loader.load()
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+splits = text_splitter.split_documents(docs)
+
+# Embed
+vectorstore = FAISS.from_documents(documents=splits, embedding=OllamaEmbeddings(model="nomic-embed-text"))
+
+#### RETRIEVAL and GENERATION ####
+
+retriever = vectorstore.as_retriever()
+prompt = hub.pull("rlm/rag-prompt")
+llm = ChatOllama(model="llama3.2:1b", temperature=0)
+
+# Post-processing
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+# Chain
+rag_chain = (
+    {"context": retriever | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+# Question
+response = rag_chain.invoke("What is Task Decomposition?")
+print(response)
