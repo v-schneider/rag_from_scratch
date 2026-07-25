bd29aa3b87e2e30d588102afd4cdede4f5c4bf54
v-schneider <valentin.schneider@mail.de>
2026-07-25 23:12:03 +0200

    Add main function.

--- rag_from_scratch.py
+++ rag_from_scratch.py
@@ -16,54 +16,61 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langsmith.utils import LangSmithMissingAPIKeyWarning
 
-warnings.filterwarnings(
-    "ignore",
-    category=LangSmithMissingAPIKeyWarning
-)
+def main():
+    # Ignore LangSmith Warning
+    warnings.filterwarnings(
+        "ignore",
+        category=LangSmithMissingAPIKeyWarning
+    )
 
-#### INDEXING ####
+    #### INDEXING ####
 
-# Load Documents
-loader = WebBaseLoader(
-    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
-    bs_kwargs=dict(
-        parse_only=SoupStrainer(
-            class_=("post-content", "post-title", "post-header")
+    # Load Documents
+    loader = WebBaseLoader(
+        web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+        bs_kwargs=dict(
+            parse_only=SoupStrainer(
+                class_=("post-content", "post-title", "post-header")
+            )
         )
     )
-)
-docs = loader.load()
+    docs = loader.load()
 
-# Split
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-splits = text_splitter.split_documents(docs)
+    # Split
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
 
-# Embed
-vectorstore = FAISS.from_documents(documents=splits, embedding=OllamaEmbeddings(model="nomic-embed-text"))
+    # Embed
+    vectorstore = FAISS.from_documents(documents=splits, embedding=OllamaEmbeddings(model="nomic-embed-text"))
 
-#### RETRIEVAL and GENERATION ####
+    #### RETRIEVAL and GENERATION ####
 
-retriever = vectorstore.as_retriever()
-prompt = hub.pull("rlm/rag-prompt")
-llm = ChatOllama(model="llama3.2:1b", temperature=0)
+    retriever = vectorstore.as_retriever()
+    prompt = hub.pull("rlm/rag-prompt")
+    llm = ChatOllama(model="llama3.2:1b", temperature=0)
+
+    # Chain
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    # Question
+    question = "What is Task Decomposition?"
+    start_time = time.perf_counter()
+    answer = rag_chain.invoke(question)
+    end_time = time.perf_counter()
+    generation_time = end_time - start_time
+
+    print("Answer:", answer)
+    print("Generation Time:", generation_time)
 
 # Post-processing
 def format_docs(docs):
     return "\n\n".join(doc.page_content for doc in docs)
 
-# Chain
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
 
-# Question
-start_time = time.perf_counter()
-answer = rag_chain.invoke("What is Task Decomposition?")
-end_time = time.perf_counter()
-generation_time = end_time - start_time
-
-print("Answer:", answer)
-print("Generation Time:", generation_time)
+if __name__ == "__main__":
+    main()
